3ac84e075f4f8a2802ea67a60a48446d46d4d28d
Pedro Javier Pablo Pascual <pedrojavierpablopascual03@gmail.com>
2024-07-22 15:21:38 -0500

    Pablo Examen Final-Parte 2 - Base de Datos

--- proyecto_final/miapp/models.py
+++ proyecto_final/miapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido_paterno = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=200)
+    SEXO_CHOICES = [
+        ('M', 'Masculino'),
+        ('F', 'Femenino'),
+    ]
+    sexo = models.CharField(max_length=1, choices=SEXO_CHOICES)
+    fecha_de_registro = models.DateTimeField(auto_now_add=True)
